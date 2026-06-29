6d20ff722976ebac50401369ce1b30eed88b3394
speedloan88 <speedloan88@gmail.com>
2026-06-29 17:49:58 +0800

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  'https://swnshjqydlazsqymchew.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3bnNoanF5ZGxhenNxeW1jaGV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MTcwMTEsImV4cCI6MjA5ODI5MzAxMX0.hXadSj75dBRNsKz8IuORzQSN5hqu2c4oO97Ddimt-Y4'
 );
